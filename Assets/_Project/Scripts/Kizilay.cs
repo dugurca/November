0c7a24f4bca303eeace77ded56be308e31d27dda
Deniz Ugurca <dugurca@gmail.com>
2016-11-12 19:03:36 +0300

    init

---
+++ Assets/_Project/Scripts/Kizilay.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kizilay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    float x = Random.value;
+        Debug.Log(x);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/_Project/Scripts/ShootBall.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootBall : MonoBehaviour
+{
+    public GameObject Ball;
+
+    void Shoot()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject ball = Instantiate(Ball);
+            ball.transform.position = this.transform.position;
+            Vector3 forceDir = hit.point - transform.position;
+            ball.GetComponent<Rigidbody>().velocity = forceDir * 5f;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetMouseButtonDown(0))
+	    {
+	        Shoot();
+	    }
+	}
+}
